d9e941255c8acb1675491d5f5630a97be9a8a584
Your Name <you@example.com>
2017-08-05 00:35:30 +0530

    Project1_spychat

--- spychat/main.py
+++ spychat/main.py
@@ -1,1 +1,6 @@
-print "hello"
+print "Let\'s get started..."
+spy_name = raw_input("What is your name ? :")
+spy_salutation = raw_input("What should we call you ? : ")
+spy_name = spy_salutation + " " + spy_name
+print "Welcome " + spy_name + " Glad to have you back with us"
+

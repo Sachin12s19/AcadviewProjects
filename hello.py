c79d091a7ee9c4a8bfd94bf76745a7a61989ea97
Your Name <you@example.com>
2017-09-02 19:27:51 +0530

    Use Of Placeholders

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print "hello world"
+print "what\'s up ? //\\\ Hello world"
+number = int(raw_input(" Enter any number : "))
+if number%2 == 0:
+
+    print "even number " +str (number)
+
+else:
+    print "odd number " +str (number)
--- spychat/main.py
+++ spychat/main.py
@@ -1,6 +1,44 @@
 print "Let\'s get started..."
 spy_name = raw_input("What is your name ? :")
-spy_salutation = raw_input("What should we call you ? : ")
-spy_name = spy_salutation + " " + spy_name
-print "Welcome " + spy_name + " Glad to have you back with us"
+
+if len(spy_name) > 0:
+        # logic will be here if condition is true
+
+        spy_salutation = raw_input("What should we call you ? : ")
+        spy_name = spy_salutation + " " + spy_name
+        print "Welcome %s  Glad to have you back with us.we would like to know more about you." %(spy_name)
+else:
+    print "invalid user....!!!"
+
+spy_age=int(raw_input("Enter your age :"))
+
+if spy_age > 12 and spy_age < 55:
+
+    print("you are iligible")
+    spy_rating = float(raw_input("What is your spy rating :"))
+    if spy_rating > 4.7 and spy_rating < 10:
+        print"You are a top class spy"
+
+    elif spy_rating >= 3.5 and spy_rating <= 4.5:
+        print "You have good face reading skills "
+
+    elif spy_rating >= 2.5 and spy_rating <= 3.5:
+        print "you are growing too fast"
+
+    else:
+        print "you have to improve your skills"
+
+    spy_is_online = True
+    print "Authentication complete Welcome %s %s age:  %d  has a rating of : %f." % (
+    spy_salutation, spy_name, spy_age, spy_rating)
+
+else:
+    print("you are not iligible")
+
+
+
+
+
+
+
 
